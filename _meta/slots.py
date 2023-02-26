8dc96b160817f0551a96d8aaedced302303ec1af
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2023-02-26 00:34:54 -0500

    Add files via upload

---
+++ _meta/slots.py
@@ -0,0 +1,12 @@
+def slotsFind():
+    print("List Items: Here are The Items in Your Current Slots.")
+
+
+
+def slotsID():
+    print("ID List: Here are The Slot ID's")
+
+
+
+def slotsEdit():
+    print("Slot Editor: Here You Can Edit Your Currently Slotted Items.")
